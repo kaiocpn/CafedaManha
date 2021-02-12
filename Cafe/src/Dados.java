aa3d6da39dc975d848eb9a779afe10f3619d1d73
Kaio Nascimento <Kaio Nascimento@DESKTOP-CSP-002>
2021-02-12 15:00:03 -0300

    Cafe

---
+++ Cafe/src/Dados.java
@@ -0,0 +1,49 @@
+public class Dados {
+    
+   private String nome;
+   
+   private int senha;
+   
+   private String cpf;
+   
+   private String prato;
+    
+   
+   public Dados(){
+   
+   
+   }
+   
+   public String getNome(){
+   
+   return nome;
+   }
+   
+   public void setNome(String name){
+   
+   nome = name;
+   }
+   
+   public String getCpf(){
+   
+   return cpf;
+   }
+   
+   public void setCpf(String log ){
+   
+   cpf = log;
+   }
+   
+   public String getPrato(){
+   
+   return prato;
+   }
+   
+   public void setPrato(String pra){
+   
+   prato = pra;
+   }
+   
+   
+   
+}
